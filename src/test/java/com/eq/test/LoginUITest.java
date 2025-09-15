47dd31c4d56c9049d525bfe4c02b4b5c786e2090
balaji-githubstore <balajidinakaran1@gmail.com>
2025-09-15 19:47:51 +0530

    Added two @Test methods

---
+++ src/test/java/com/eq/test/LoginUITest.java
@@ -0,0 +1,45 @@
+package com.eq.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginUITest {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	}
+	
+	@AfterMethod
+	public void teardown()
+	{
+		driver.quit();
+	}
+
+	@Test
+	public void titleTest() {
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, "OrangeHRM");
+	}
+
+	@Test
+	public void headerTest() {
+		String actualHeader = driver.findElement(By.xpath("//h5")).getText();
+		Assert.assertEquals(actualHeader, "Login");
+	}
+
+}

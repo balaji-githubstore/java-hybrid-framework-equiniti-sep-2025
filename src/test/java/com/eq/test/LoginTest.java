016b61b8c4fd29669f552d03cec7330bf69eafda
balaji-githubstore <balajidinakaran1@gmail.com>
2025-09-16 16:57:58 +0530

    Added ValidLoginTest

--- src/test/java/com/eq/test/LoginTest.java
+++ src/test/java/com/eq/test/LoginTest.java
@@ -1,6 +1,7 @@
 package com.eq.test;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.eq.base.AutomationWrapper;
@@ -10,10 +11,12 @@ public class LoginTest extends AutomationWrapper {
 	@Test
 	public void validLoginTest() {
 		driver.findElement(By.name("username")).sendKeys("Admin");
-		// enter password as admin123
-		// click on login
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 
-		// Assert the text -- Time at Work
+		String actualText=driver.findElement(By.xpath("//p[contains(normalize-space(),'Time at')]")).getText();
+		Assert.assertEquals(actualText, "Time at Work");
 	}
 
+	
 }

570d893bd687a3ffd228f6d3822398639ce98aee
balaji-githubstore <balajidinakaran1@gmail.com>
2025-09-16 17:10:49 +0530

    Added invalid credentials

--- src/test/java/com/eq/test/LoginTest.java
+++ src/test/java/com/eq/test/LoginTest.java
@@ -12,11 +12,21 @@ public class LoginTest extends AutomationWrapper {
 	public void validLoginTest() {
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		driver.findElement(By.xpath("//button[contains(normalize-space(),'Login') or contains(normalize-space(),'登录')]")).click();
 
 		String actualText=driver.findElement(By.xpath("//p[contains(normalize-space(),'Time at')]")).getText();
 		Assert.assertEquals(actualText, "Time at Work");
 	}
 
-	
+	@Test
+	public void invalidLoginTest()
+	{
+		driver.findElement(By.name("username")).sendKeys("john");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[contains(normalize-space(),'Login') or contains(normalize-space(),'登录')]")).click();
+
+		//Assert the Invalid credentials
+		String actualError=driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
+		Assert.assertEquals(actualError, "Invalid credentials");
+	}
 }
